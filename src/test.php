49f4bda777edd04ae646a3a4bb910c380eddef86
David <daviderler1995@gmail.com>
2016-04-01 01:15:46 +0200

    at 8 min

--- src/test.php
+++ src/test.php
@@ -1,19 +1,33 @@
 
-<!DOCTYPE HTML>
 <html>
-<header>
-</header>
-<body>
-<h1>Das ist ein Test</h1>
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: David
- * Date: 01.04.2016
- * Time: 00:34
- */?>
-</body>
+    <head>
+        <title>My website</title>
+        <link rel="stylesheet" type="text/css" href="styles/test.css"/>
 
-
-
+    </head>
+    <body>
+        <div id="container">
+            <div id="header">
+                <h1>First Test</h1>
+            </div>
+            <div id="content">
+                <div id="nav">
+                    <h3>Navigation</h3>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div id="main">
+                    <h2>Homepage</h2>
+                    <p>Das ist nur ein test</p>
+                    <P>nur was zum testen</P>
+                </div>
+            </div>
+            <div id="footer">
+                naja noch irgendwas
+            </div>
+        </div>
+    </body>
 </html>
